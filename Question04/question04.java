f20d112bc58ca17a19123d09d93b75706f4a648b
Lasanga Dissanayaka <129869155+lasanga890@users.noreply.github.com>
2025-03-15 18:09:02 +0000

    create a java code for print odd numbers between 1 -20

---
+++ Question04/question04.java
@@ -0,0 +1,9 @@
+package Question04;
+
+public class question04 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i=i+2) {
+            System.out.println(i);
+        }
+    }
+}
